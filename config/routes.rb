d1ce7be077c9c9bc530c572f050a7dfec80a25f3
k941837 <55436678+k941837@users.noreply.github.com>
2019-12-10 17:13:34 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,22 @@ Rails.application.routes.draw do
   end
 
   resources :images, only: [:destroy]
-
-  resources :users
+  resources :users, only: [:index, :show, :new, :update, :edit] do
+    collection do
+      get "new1", to: "users#new1"
+      get "new2", to: "users#new2"
+      get "new3", to: "users#new3"
+      get "new4", to: "users#new4"
+      get "new5", to: "users#new5"
+      get "logout", to: "users#logout"
+      get "payment"
+      get "identification"
+    end
+    member do
+      get "listing"
+      get "profile"
+    end
+  end
   resources :categories, only: [:index, :show]
   resources :brands, only: [:index, :show]
 
